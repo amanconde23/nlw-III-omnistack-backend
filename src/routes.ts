54a654542ba1acecee05c461c3af3aa13d27b76b
amanconde23 <amanconde23@gmail.com>
2020-10-14 00:13:58 -0300

    dia 2 nlw - desenvolvimento do backend

---
+++ src/routes.ts
@@ -0,0 +1,15 @@
+import {Router} from 'express';
+import multer from 'multer';
+
+import uploadConfig from './config/upload';
+import OrphanagesController from './controllers/OrphanagesController';
+
+const routes = Router();
+const upload = multer(uploadConfig);
+
+routes.get('/orphanages', OrphanagesController.index);
+routes.get('/orphanages/:id', OrphanagesController.show);
+// upload.array permite o upload de mais de uma img
+routes.post('/orphanages', upload.array('images'), OrphanagesController.create);
+
+export default routes;
